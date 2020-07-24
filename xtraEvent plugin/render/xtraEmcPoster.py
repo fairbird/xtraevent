f290ef43186af8eda706ec50474e707897060b6e
digiteng <digiteng@gmail.com>
2020-07-24 13:00:46 +0300

    Update xtraEmcPoster.py

--- xtraEvent plugin/render/xtraEmcPoster.py
+++ xtraEvent plugin/render/xtraEmcPoster.py
@@ -2,11 +2,12 @@
 # by digiteng...07.2020
 # <widget source="Service" render="xtraEmcPoster" position="0,0" size="185,278" zPosition="0"
 from Renderer import Renderer
-from enigma import ePixmap, eTimer, loadJPG
+from enigma import ePixmap, eTimer, ePicLoad
+from Components.AVSwitch import AVSwitch
 from Components.Sources.ServiceEvent import ServiceEvent
 from Components.Sources.CurrentService import CurrentService
 from Components.config import config
-import os
+from Tools.Directories import fileExists
 
 try:
 	from Plugins.Extensions.xtraEvent.xtra import xtra
@@ -18,8 +19,7 @@ class xtraEmcPoster(Renderer):
 
 	def __init__(self):
 		Renderer.__init__(self)
-		self.pstrNm = ''
-		self.evntNm = ''
+
 
 	GUI_WIDGET = ePixmap
 	def changed(self, what):
@@ -31,20 +31,38 @@ class xtraEmcPoster(Renderer):
 			self.delay()
 
 	def showPoster(self):
+		movieNm = ""
 		try:
-
 			service = self.source.getCurrentService()
 			if service:
 				evnt = service.getPath()
 				movieNm = evnt.split('-')[-1].split(".")[0].strip()
-				pstrNm = pathLoc + "xtraEvent/EMC/{}-poster.jpg".format(movieNm)
-				if os.path.exists(pstrNm):
-					self.instance.setPixmap(loadJPG(pstrNm))
-					self.instance.show()
-
+				pstrNm = "{}xtraEvent/EMC/{}-poster.jpg".format(pathLoc, movieNm)
+				if not fileExists(pstrNm):
+					pstrNm = "/usr/lib/enigma2/python/Plugins/Extensions/xtraEvent/pic/noCvr.jpg"
 				else:
-					self.instance.hide()
-
+					pstrNm = "{}xtraEvent/EMC/{}-poster.jpg".format(pathLoc, movieNm)
+				size = self.instance.size()
+				self.picload = ePicLoad()
+				sc = AVSwitch().getFramebufferScale()
+				if self.picload:
+					self.picload.setPara((size.width(),
+					size.height(),
+					sc[0],
+					sc[1],
+					False,
+					1,
+					'#00000000'))
+				result = self.picload.startDecode(pstrNm, 0, 0, False)
+				if result == 0:
+					ptr = self.picload.getData()
+					if ptr != None:
+						self.instance.setPixmap(ptr)
+						self.instance.show()
+				del self.picload
+			else:
+				self.instance.hide()
+				return
 		except:
 			return
 
